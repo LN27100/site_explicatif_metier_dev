2ddd1a229c328f4ae1dfd7361d9a9c46da83f6fb
LN27100 <heloceinlove@hotmail.fr>
2024-09-23 18:07:07 +0200

    ajout de commentaires

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,10 @@
+
+// fonction simple de recherche
+
 function afficherRecherche() {
+      // Récupère la valeur de l'élément HTML ayant l'ID "recherche" (par exemple, un champ de saisie).
     var valeur = document.getElementById("recherche").value;
+    // Modifie le contenu de l'élément HTML avec l'ID "resultat" pour afficher la chaîne de texte
+    // combinée avec la valeur saisie par l'utilisateur.
     document.getElementById("resultat").innerHTML = "Vous avez recherché : " + valeur;
   }
